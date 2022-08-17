d87bc1cdc72c61906eed4f0c392ffa0d1687fdb9
Vladimir Volek <volek.vladimir@gmail.com>
2022-08-17 13:05:21 +0200

    feat: getTimeFromSlot

---
+++ src/cardano.ts
@@ -0,0 +1,3 @@
+export const getTimeFromSlot = (slotNumber: number) => {
+  return 1_596_491_091 + (slotNumber - 4_924_800);
+};
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as openApi from './open-api';
 import * as validation from './validation';
 import * as fastify from './fastify';
 import * as pagination from './pagination';
+import * as cardano from './cardano';
 
-export { openApi, validation, fastify, pagination };
+export { openApi, validation, fastify, pagination, cardano };
